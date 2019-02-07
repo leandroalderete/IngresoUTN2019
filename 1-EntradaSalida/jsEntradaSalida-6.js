29cef8e41f87cf8219d03079d6905e5eda50875e
leandroalderete <47337964+leandroalderete@users.noreply.github.com>
2019-02-06 22:15:35 -0300

    ultimo 6 feb
    6 feb

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,9 +3,9 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-var num1;
+var num1
 
-var num2;
+var num2
 
 var resultado
 
@@ -13,13 +13,13 @@ num1= document.getElementById ("numeroUno").value;
 
 num2= document.getElementById ("numeroDos").value;
 
+num1= parseInt (num1);
 
+num2= parseInt (num2);
 
-resultado = num1 + num2;
+resultado= num1 + num2;
 
-alert ("la suma es " + resultado);
-
-
+alert ("la suma es " + resultado); 
 
 
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,13 +3,29 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	var num1
+    var num2
+    var resultado
+    num1= document.getElementById ("numeroUno").value;
+    num2= document.getElementById ("numeroDos").value;
+    num1= parseInt (num1);
+    num2= parseInt (num2);
+    resultado = num1 + num2;
+    alert ("la suma es " + resultado);
 	
 }
 
 function restar()
 {
-	
+	var num1
+    var num2
+    var resultado
+    num1= document.getElementById ("numeroUno").value;
+    num2= document.getElementById ("numeroDos").value;
+    num1=  (num1);
+    num2=  (num2);
+    resultado = num1 - num2;
+    alert ("la resta es " - resultado);
 }
 
 function multiplicar()
@@ -19,6 +35,13 @@ function multiplicar()
 
 function dividir()
 {
-	
+    var num1;
+    
+    var num2;
+
+    var resultado
+
+
+ 
 }
 
